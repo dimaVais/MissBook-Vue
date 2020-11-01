b53f1df903dbfd88a946b3244f83fce8ead95ed6
dimaVais <dimavais300@gmail.com>
2020-11-01 20:07:04 +0200

    add deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/vue-missBook/'
-      : '/'
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//       ? '/vue-missBook/'
+//       : '/'
+
+      
+//   }
+  module.exports = {
+    publicPath: '/vue-missBook/'
   }
