8dafaec2bdb36c1c733ef51b76246954dc3bbf84
dimaVais <dimavais300@gmail.com>
2020-11-03 16:00:00 +0200

    fix export

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/vue-missBook/' :
-        '/'
-}
+export const publicPath = process.env.NODE_ENV === 'production' ?
+    '/vue-missBook/' :
+    '/';
 //   module.exports = {
 //     publicPath: '/vue-missBook/'
 //   }
